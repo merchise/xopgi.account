be3facda4dc58eefb999050c0860a7b0fbdd1e7d
Manuel Vázquez Acosta <mva.led@gmail.com>
2014-12-22 16:47:51 -0500

    Jump to 1.8.

--- xopgi/xopgi_account/__openerp__.py
+++ xopgi/xopgi_account/__openerp__.py
@@ -14,7 +14,7 @@
 
 {
     "name": "Accounting (extended - xopgi)",
-    "version": "1.7",
+    "version": "1.8",
     "author": "Merchise Autrement",
     "website": "http://xopgi.merchise.org/addons/xopgi_account",
     "category": "Accounting",

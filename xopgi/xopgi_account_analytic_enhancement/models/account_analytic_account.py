40cc42578f90db0e79bd5ec793bb8715ec93fae8
Andro <andro@merchise.org>
2015-07-30 17:32:16 -0400

    Added Operations views and logic including Commission% and Commission

--- xopgi/xopgi_account_analytic_enhancement/models/account_analytic_account.py
+++ xopgi/xopgi_account_analytic_enhancement/models/account_analytic_account.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
+import math
 from openerp import api, fields, models
 
 
@@ -9,17 +10,35 @@ class AccountAnalyticAccount(models.Model):
 
     percentage_margin = fields.Float(
         string='Margin %', help='Percentage margin related to credit.',
-        compute='_compute_percentage_margin')
+        compute='_compute_commission')
+
+    percentage_commission = fields.Float(
+        string='Commission %', help='Percentage commission related to profit.',
+        compute='_compute_commission')
+
+    commission = fields.Float(
+        string='Commission', help='Commission related to profit.',
+        compute='_compute_commission')
 
     @api.one
-    @api.depends('debit', 'credit')
-    def _compute_percentage_margin(self):
-        if self.credit == 0:
-            if self.debit == 0:
-                self.percentage_margin = 0
-            else:
-                self.percentage_margin = 100
+    @api.depends('debit', 'balance')
+    def _compute_commission(self):
+        if self.balance == 0:
+            self.percentage_margin = 0
         else:
-            self.percentage_margin = self.debit * 100 / self.credit - 100
-            if self.percentage_margin > 100:
-                self.percentage_margin = 100
+            self.percentage_margin = self.balance / self.debit * 100
+
+        percentage_abs = math.fabs(self.percentage_margin / 100.0)
+
+        if percentage_abs >= 0.15:
+            factor = percentage_abs / 0.5
+            if math.floor(factor) < 1:
+                if self.debit <= 4000:
+                    factor += 0.2
+            else:
+                factor = 1
+            self.percentage_commission = factor * 5
+        else:
+            self.percentage_commission = 0
+
+        self.commission = self.percentage_commission * self.balance / 100

64d257e8e830aa51c5d264fcfdf3bf31ae7de62d
Manuel Vázquez Acosta <manu@merchise.org>
2015-10-30 15:04:18 -0400

    Add the copyright notice.

--- xopgi/xopgi_payment_extension/wizard/voucher_wizard.py
+++ xopgi/xopgi_payment_extension/wizard/voucher_wizard.py
@@ -1,6 +1,23 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# voucher_wizard
+# ---------------------------------------------------------------------
+# Copyright (c) 2015 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2015-10-30
 
-from openerp import _, models, fields, api
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+
+from openerp import models, fields, api
 
 
 class VoucherWizard(models.TransientModel):

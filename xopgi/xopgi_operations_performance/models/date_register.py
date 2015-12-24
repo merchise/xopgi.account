55f3ebb7b9c013b0d6c280b3815ac532cb631ef2
Manuel Vázquez Acosta <manu@merchise.org>
2015-12-24 12:41:15 -0500

    Fix flakes.

--- xopgi/xopgi_operations_performance/models/date_register.py
+++ xopgi/xopgi_operations_performance/models/date_register.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
 
-from openerp import api, fields, models
+from openerp import fields, models
 
 
 class SaleOrder(models.Model):
--- xopgi/xopgi_payment_extension/wizard/__init__.py
+++ xopgi/xopgi_payment_extension/wizard/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import voucher_wizard
+from . import voucher_wizard  # noqa

4e92af08bce4a008e609343365850ebfdc248fb0
Andro Font <andro@merchise.org>
2016-02-22 12:07:39 -0500

    [ADD] Currency rate widget

---
+++ xopgi/xopgi_currency_rate/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Currency Rate Widget",
+
+    "summary": """
+        Show currency exchange an provide a converter.""",
+
+    "description": """
+        Show currency exchange an provide a converter.
+    """,
+
+    "author": "Merchise Autrement",
+
+    "category": "Accounting",
+    "version": "1.0",
+
+    "depends": ["base", "web"],
+    "data": [
+        "views/currency.xml"
+    ],
+    "qweb": ["static/src/xml/*.xml"]
+}

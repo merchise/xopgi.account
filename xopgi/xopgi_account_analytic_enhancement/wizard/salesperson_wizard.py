ee597c57f3672b9b95ce2d8c85de0d732b8f9e49
Manuel Vázquez Acosta <manu@merchise.org>
2016-02-29 18:26:33 -0500

    Add proper copyright notice.

--- xopgi/xopgi_account_analytic_enhancement/wizard/salesperson_wizard.py
+++ xopgi/xopgi_account_analytic_enhancement/wizard/salesperson_wizard.py
@@ -1,4 +1,16 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# salesperson_wizard
+# ---------------------------------------------------------------------
+# Copyright (c) 2015-2016 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+
 from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)

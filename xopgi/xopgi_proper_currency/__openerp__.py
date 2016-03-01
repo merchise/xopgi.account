afe8e5a03e6eabba4e268c860bc49d3601ea6c28
Manuel Vázquez Acosta <manu@merchise.org>
2016-03-01 15:11:16 -0500

    Revert "branch develop: Remove unneeded field."
    This reverts commit 721391f54b723b1ac6462453f81ba5d2972d77c7.

--- xopgi/xopgi_proper_currency/__openerp__.py
+++ xopgi/xopgi_proper_currency/__openerp__.py
@@ -14,7 +14,7 @@
 
 dict(
     name='xopgi_proper_currency',
-    version="1.15",
+    version="1.16",
     author="Merchise Autrement",
     website="http://merchise-autrement.gitlab.io/xopgi/",
     category="Hidden",

dbae6b2e51c5f9a76a474e037a12aa3ec70e3e98
Pedro Torres <pedro@merchise.org>
2016-06-03 00:16:22 -0400

    Including module data files.

--- xopgi/xopgi_distance_calculator/__openerp__.py
+++ xopgi/xopgi_distance_calculator/__openerp__.py
@@ -12,12 +12,14 @@ Distance Calculator Widget
 
 Allows users to know the distance between cities and places.
         """,
+    'depends': ['base', 'web'],
     'data': [
         'security/ir.model.access.csv',
+        'data/res.route.points.xml',
+        'data/res.routes.xml',
         'views/distance_calculator.xml',
         "views/travel_routes.xml",
     ],
-    'depends': ['base', 'web'],
     'qweb': ['static/src/xml/distance_calculator.xml'],
     'application': False,
     'installable': (8, 0) <= ODOO_VERSION_INFO < (9, 0),   # noqa

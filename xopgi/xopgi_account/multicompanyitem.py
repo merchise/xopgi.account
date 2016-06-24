b68f44e1adc9ac7dd556fc32bd97e1ffa975b06c
Manuel Vázquez Acosta <manu@merchise.org>
2016-06-24 11:36:19 -0400

    Add the logo.

--- xopgi/xopgi_account/multicompanyitem.py
+++ xopgi/xopgi_account/multicompanyitem.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # xopgi.xopgi.xopgi_account.journal
 # ---------------------------------------------------------------------
-# Copyright (c) 2013-2016 Merchise Autrement
+# Copyright (c) 2013-2016 Merchise Autrement [~º/~]
 # All rights reserved.
 #
 #
--- xopgi/xopgi_operations_performance/models/operations_performance_report.py
+++ xopgi/xopgi_operations_performance/models/operations_performance_report.py
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------------------
 # operations_performance_report.py
 # ---------------------------------------------------------------------
-# Copyright (c) 2015, 2016 Merchise Autrement and Contributors
+# Copyright (c) 2015, 2016 Merchise Autrement [~º/~] and Contributors
 # All rights reserved.
 #
 # This is free software; you can redistribute it and/or modify it under the

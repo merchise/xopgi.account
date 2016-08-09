8e441110fe3031fbdd66cb6fe51975cf87f684d7
Manuel Vázquez Acosta <manu@merchise.org>
2016-08-09 13:28:29 -0400

    Document the models.

--- xopgi/xopgi_operations_performance/models/date_register.py
+++ xopgi/xopgi_operations_performance/models/date_register.py
@@ -7,6 +7,8 @@ from openerp import fields, models
 
 
 class SaleOrder(models.Model):
+    # Save the datetime when a sale order is sent via the 'Send to' action.
+    # See also the workflows.
     _inherit = "sale.order"
 
     send_date = fields.Datetime(string="Send Date", help="Order send date",
@@ -17,6 +19,7 @@ class SaleOrder(models.Model):
 
 
 class PurchaseRequisition(models.Model):
+    # Save the datetime when a purchase requisition is open for bids.
     _inherit = "purchase.requisition"
 
     call_date = fields.Datetime(string="Call Date",
@@ -28,6 +31,8 @@ class PurchaseRequisition(models.Model):
 
 
 class PurchaseOrder(models.Model):
+    # Save the datetime when a purchase order is sent to the provider via the
+    # 'Sent to' action.
     _inherit = "purchase.order"
 
     send_date = fields.Datetime(string="Send Date",

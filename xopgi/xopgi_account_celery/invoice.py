a8864231462ad3807a3121f1e1205874ce7542ca
Manuel Vázquez Acosta <manu@merchise.org>
2017-02-27 17:38:24 -0500

    Use celery when confirming several invoices.
    If at least an invoice is validated, confirm the work partially.  We tried to
    issue another background job, but that failed.

--- xopgi/xopgi_account_celery/invoice.py
+++ xopgi/xopgi_account_celery/invoice.py
@@ -16,17 +16,18 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-
-from xoeuf.ui import RELOAD_UI
+from xoeuf.ui import RELOAD_UI, CLOSE_WINDOW
 
 try:
     from odoo import models, api, _
-    from odoo.jobs import Deferred, report_progress
+    from odoo.jobs import Deferred, report_progress, until_timeout
     from odoo.addons.web_celery import WAIT_FOR_TASK
+    from odoo.exceptions import ValidationError
 except ImportError:
     from openerp import models, api, _
-    from openerp.jobs import Deferred, report_progress
+    from openerp.jobs import Deferred, report_progress, until_timeout
     from openerp.addons.web_celery import WAIT_FOR_TASK
+    from openerp.exceptions import ValidationError
 
 
 class ValidateInvoice(models.Model):
@@ -38,13 +39,43 @@ class ValidateInvoice(models.Model):
 
     @api.multi
     def _do_validate(self):
-        report_progress(
-            _("Validating the invoices. This may take several "
-              "minutes depending on the length of the invoices "),
-            valuemin=0,
-            valuemax=len(self)
+        # Do the real work inside an iterator so that we can use the
+        # until_timeout.
+        def _validate():
+            report_progress(
+                _("Validating the invoices. This may take several "
+                  "minutes depending on the length of the invoices "),
+                valuemin=0,
+                valuemax=len(self)
+            )
+            records = self.sorted(lambda invoice: len(invoice.invoice_line))
+            for progress, record in enumerate(records):
+                report_progress(progress=progress)
+                with self.env.cr.savepoint():
+                    # Since the soft time-limit may occur after the first
+                    # invoice was validate, but before the second finishes, we
+                    # must ensure all DB changes for each invoice is
+                    # 'atomically' done or not done at all.
+                    record.signal_workflow('invoice_open')
+                yield record
+
+        res = list(until_timeout(_validate()))
+        if not res and any(self):
+            raise ValidationError(_("No invoice could be validated"))
+        else:
+            return RELOAD_UI
+
+
+class ConfirmInvoices(models.TransientModel):
+    _name = _inherit = 'account.invoice.confirm'
+
+    @api.multi
+    def invoice_confirm_with_celery(self):
+        invoice_ids = self.env.context.get('active_ids', []) or []
+        invoices = self.env['account.invoice'].browse(invoice_ids).filtered(
+            lambda invoice: invoice.state in ('draft', 'proforma', 'proforma2')
         )
-        for progress, record in enumerate(self):
-            report_progress(progress=progress)
-            record.signal_workflow('invoice_open')
-        return RELOAD_UI
+        if any(invoices):
+            return invoices.invoice_open_with_celery()
+        else:
+            return CLOSE_WINDOW

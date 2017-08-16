64801d6376391130138cae48bd987cdb07065efb
Manuel Vázquez Acosta <manu@merchise.org>
2017-08-16 17:42:54 -0400

    Format to improve reading.

--- xopgi/xopgi_account_analytic_enhancement/wizard/salesperson_wizard.py
+++ xopgi/xopgi_account_analytic_enhancement/wizard/salesperson_wizard.py
@@ -35,19 +35,23 @@ class PrimaryInstructorWizard(models.TransientModel):
             self._context.get("active_id")).primary_salesperson_id
 
     primary_salesperson_id = fields.Many2one(
-        "res.users", string="Salesperson", required=True,
-        default=_get_salesperson)
+        "res.users",
+        string="Salesperson",
+        required=True,
+        default=_get_salesperson
+    )
     analytic_account_ids = fields.Many2many(
-        "account.analytic.account", string="Operations", required=True,
+        "account.analytic.account",
+        string="Operations",
+        required=True,
         domain=[("type", "=", "contract"), ("state", "=", "close"),
-                ("supplier_invoice_id", "=", False)])
+                ("supplier_invoice_id", "=", False)]
+    )
 
     @api.onchange("primary_salesperson_id")
     def _onchange_primary_salesperson_id(self):
-        self.analytic_account_ids = self.env[
-            "account.analytic.account"].search(
-            [("primary_salesperson_id.id", "=",
-              self.primary_salesperson_id.id), ("type", "=", "contract"),
+        self.analytic_account_ids = self.env["account.analytic.account"].search(
+            [("primary_salesperson_id.id", "=", self.primary_salesperson_id.id), ("type", "=", "contract"),
              ("state", "=", "close"), ("supplier_invoice_id", "=", False)])
 
     @api.multi
@@ -63,30 +67,31 @@ class PrimaryInstructorWizard(models.TransientModel):
         # which is under tight timeout restriction (because the same
         # restriction applies to Cron and HTTP workers).  Jobs are allowed to
         # take longer.
-        Deferred(self._name, cr, uid, 'generate_supplier_invoice_cron',
-                 context=context)
+        Deferred(self.generate_supplier_invoice_cron)
 
+    @api.model
     def generate_supplier_invoice_cron(self, cr, uid, context=None):
-        with api.Environment.manage():
-            self.env = api.Environment(cr, uid, {})
-            commission_ready = self.env["account.analytic.account"].search(
-                [('type', '=', 'contract'), ('state', '=', 'close'),
-                 ('supplier_invoice_id', '=', False)])
-            salesperson_commissions = {}
-            for commission in commission_ready:
-                salesperson = commission.primary_salesperson_id
-                if salesperson:
-                    sale_partner = salesperson.partner_id
-                    comm = salesperson_commissions.setdefault(
-                        sale_partner.id, []
-                    )
-                    comm.append(commission)
-            for salesperson_key in salesperson_commissions:
-                partner = self.env["res.partner"].browse([salesperson_key])
-                account_id = partner.property_account_payable.id
-                self._supplier_invoice_generator(partner, account_id,
-                                                 salesperson_commissions[
-                                                     salesperson_key])
+        commission_ready = self.env["account.analytic.account"].search(
+            [('type', '=', 'contract'), ('state', '=', 'close'),
+             ('supplier_invoice_id', '=', False)]
+        )
+        salesperson_commissions = {}
+        for commission in commission_ready:
+            salesperson = commission.primary_salesperson_id
+            if salesperson:
+                sale_partner = salesperson.partner_id
+                comm = salesperson_commissions.setdefault(
+                    sale_partner.id, []
+                )
+                comm.append(commission)
+        for salesperson_key in salesperson_commissions:
+            partner = self.env["res.partner"].browse([salesperson_key])
+            account_id = partner.property_account_payable.id
+            self._supplier_invoice_generator(
+                partner,
+                account_id,
+                salesperson_commissions[salesperson_key]
+            )
             del self.env
 
     def _supplier_invoice_generator(self, partner, account_id,
@@ -104,10 +109,8 @@ class PrimaryInstructorWizard(models.TransientModel):
                   ('company_id', '=', self._context.get(
                       'company_id', self.env.user.company_id.id))],
                  limit=1).id})
-        supplier_invoice.message_follower_ids |= self.env[
-            "res.partner"].browse([partner.id])  # UPDATE ...
-        employees = self.env["hr.employee"].search(
-            [("user_id.partner_id", "=", partner.id)])
+        supplier_invoice.message_follower_ids |= self.env["res.partner"].browse(partner.id)  # UPDATE ...
+        employees = self.env["hr.employee"].search([("user_id.partner_id", "=", partner.id)])
         for employee in employees:
             if employee.parent_id:
                 manager = employee.parent_id.user_id.partner_id

3d46afd82fbfd09129f77b2bd77c842c66835c1a
Manuel Vázquez Acosta <manu@merchise.org>
2017-08-24 01:54:15 -0400

    Mark `xopgi_payment_extensions` ready for Odoo 9 and 10.

--- xopgi/xopgi_payment_extension/__init__.py
+++ xopgi/xopgi_payment_extension/__init__.py
@@ -5,6 +5,7 @@ from __future__ import (division as _py3_division,
 
 from xoeuf import MAJOR_ODOO_VERSION
 
+# WARNING: Don't load in Odoo 9+ since they have this feature already.
 if 8 <= MAJOR_ODOO_VERSION < 9:
     from . import models  # noqa
     from . import wizard  # noqa
--- xopgi/xopgi_payment_extension/__openerp__.py
+++ xopgi/xopgi_payment_extension/__openerp__.py
@@ -16,10 +16,17 @@ dict(
     author="Merchise Autrement",
     category='Accounting & Finance',
     version='1.0',
-    depends=['base', "account_voucher"],
+    depends=[
+        'base',
+        'account'
+    ] + (['account_voucher'] if MAJOR_ODOO_VERSION < 9 else []),  # noqa
     data=[
-        "views/account_voucher_views.xml",
-        "wizard/voucher_wizard.xml"
+        "views/%d/account_voucher_views.xml" % MAJOR_ODOO_VERSION,  # noqa
+        "wizard/%d/voucher_wizard.xml" % MAJOR_ODOO_VERSION,  # noqa
     ],
-    installable=8 <= MAJOR_ODOO_VERSION < 9,   # noqa
+
+    # Since Odoo 9, it quite unused.  Odoo 9 and 10 have the option to create
+    # a payment from invoices directly.  We mark it ready to ease the
+    # migration.
+    installable=8 <= MAJOR_ODOO_VERSION < 11,   # noqa
 )

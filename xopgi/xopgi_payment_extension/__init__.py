47f23bfe51881b03f5f383f89a1e4871b979fa93
Manuel Vázquez Acosta <manu@merchise.org>
2017-08-23 20:03:32 -0400

    Protect from loading in unsupported versions.

--- xopgi/xopgi_payment_extension/__init__.py
+++ xopgi/xopgi_payment_extension/__init__.py
@@ -3,5 +3,8 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-from . import models  # noqa
-from . import wizard  # noqa
+from xoeuf import MAJOR_ODOO_VERSION
+
+if 8 <= MAJOR_ODOO_VERSION < 9:
+    from . import models  # noqa
+    from . import wizard  # noqa

merchise/xopgi.account

commit 5f4610c6ac2e4f97d006fd23238dd48678d36c1f

Remove 'invoice' modules.
They were very similar.  Putting all in 'base.py' highlights better the
differences between the major odoo versions.

xopgi/xopgi_account_celery/base.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------
-# invoice
-# ---------------------------------------------------------------------
-# Copyright (c) 2017 Merchise Autrement [~º/~] and Contributors
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
 # All rights reserved.
 #
-# This is free software; you can redistribute it and/or modify it under the
-# terms of the LICENCE attached (see LICENCE file) in the distribution
-# package.
+# This is free software; you can do what the LICENCE file allows you to.
 #
-# Created on 2017-02-27
 
 from __future__ import (division as _py3_division,
                         print_function as _py3_print,
@@ -18,16 +13,38 @@ from __future__ import (division as _py3_division,
 
 from xoeuf.ui import CLOSE_WINDOW
 
-from xoeuf import models, api
-from xoeuf.odoo.jobs import Deferred
+from xoeuf import models, api, MAJOR_ODOO_VERSION
+from xoeuf.odoo import _
+from xoeuf.odoo.jobs import Deferred, report_progress, until_timeout
 from xoeuf.odoo.addons import web_celery
 from xoeuf.odoo.addons.web_celery import WAIT_FOR_TASK
+from xoeuf.odoo.exceptions import ValidationError
 
 
 QUIETLY_WAIT_FOR_TASK = getattr(web_celery, 'QUIETLY_WAIT_FOR_TASK',
                                 WAIT_FOR_TASK)
 
 CLOSE_PROGRESS_BAR = getattr(web_celery, 'CLOSE_FEEDBACK', None)
+
+
+# The only differences of any importance to us between Odoo 8 and Odoo 9+, is
+# how to get the invoice's lines, and how to actually perform the validation.
+if MAJOR_ODOO_VERSION == 8:
+    def get_lines(invoice):
+        return invoice.invoice_line
+
+elif MAJOR_ODOO_VERSION < 11:
+    def get_lines(invoice):
+        return invoice.invoice_line_ids
+
+
+if MAJOR_ODOO_VERSION < 10:
+    def perform_validate(invoice):
+        invoice.signal_workflow('invoice_open')
+
+else:
+    def perform_validate(invoice):
+        invoice.action_invoice_open()
 
 
 class ValidateInvoice(models.Model):
@@ -37,8 +54,39 @@ class ValidateInvoice(models.Model):
     def invoice_open_with_celery(self):
         return QUIETLY_WAIT_FOR_TASK(Deferred(self._do_validate))
 
-    # _do_validate implemented per major Odoo version in invoice8 and
-    # invoice10.
+    @api.multi
+    def _do_validate(self):
+        # Do the real work inside an iterator so that we can use the
+        # until_timeout.
+        def _validate():
+            report_progress(
+                _("Validating the invoices. This may take several "
+                  "minutes depending on the length of the invoices "),
+                valuemin=0,
+                valuemax=len(self)
+            )
+            records = self.sorted(lambda invoice: len(get_lines(invoice)))
+            for progress, record in enumerate(records):
+                report_progress(progress=progress)
+                with self.env.cr.savepoint():
+                    # Since the soft time-limit may occur after the first
+                    # invoice was validate, but before the second finishes, we
+                    # must ensure all DB changes for each invoice is
+                    # 'atomically' done or not done at all.
+                    perform_validate(record)
+                yield record
+
+        def count(iterable):
+            result = 0
+            for _ in iterable:
+                result += 1
+            return result
+
+        res = count(until_timeout(_validate()))
+        if not res and any(self):
+            raise ValidationError(_("No invoice could be validated"))
+        else:
+            return CLOSE_PROGRESS_BAR
 
 
 class ConfirmInvoices(models.TransientModel):

xopgi/xopgi_account_celery/__init__.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------
-# xopgi_account_celery
-# ---------------------------------------------------------------------
-# Copyright (c) 2017 Merchise Autrement [~º/~] and Contributors
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
 # All rights reserved.
 #
-# This is free software; you can redistribute it and/or modify it under the
-# terms of the LICENCE attached (see LICENCE file) in the distribution
-# package.
+# This is free software; you can do what the LICENCE file allows you to.
 #
-# Created on 2017-02-27
 
 from __future__ import (division as _py3_division,
                         print_function as _py3_print,
@@ -20,9 +15,3 @@ from xoeuf import MAJOR_ODOO_VERSION
 
 if 8 <= MAJOR_ODOO_VERSION < 11:
     from . import base  # noqa
-
-    if 8 <= MAJOR_ODOO_VERSION < 10:
-        from . import invoice8  # noqa
-
-    elif MAJOR_ODOO_VERSION == 10:
-        from . import invoice10  # noqa

xopgi/xopgi_account_celery/__openerp__.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------
-# __openerp__
-# ---------------------------------------------------------------------
-# Copyright (c) 2017 Merchise Autrement [~º/~] and Contributors
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
 # All rights reserved.
 #
-# This is free software; you can redistribute it and/or modify it under the
-# terms of the LICENCE attached (see LICENCE file) in the distribution
-# package.
+# This is free software; you can do what the LICENCE file allows you to.
 #
-# Created on 2017-02-09
 
 dict(
     name='xopgi_account_celery',

xopgi/xopgi_account_celery/invoice10.py
@@ -1,56 +1,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# ---------------------------------------------------------------------
-# invoice
-# ---------------------------------------------------------------------
-# Copyright (c) 2017 Merchise Autrement [~º/~] and Contributors
-# All rights reserved.
-#
-# This is free software; you can redistribute it and/or modify it under the
-# terms of the LICENCE attached (see LICENCE file) in the distribution
-# package.
-#
-# Created on 2017-02-27
-
-from __future__ import (division as _py3_division,
-                        print_function as _py3_print,
-                        absolute_import as _py3_abs_import)
-
-from xoeuf import models, api
-from xoeuf.odoo import _
-from xoeuf.odoo.jobs import report_progress, until_timeout
-from xoeuf.odoo.exceptions import ValidationError
-
-from .base import CLOSE_PROGRESS_BAR
-
-
-class ValidateInvoice(models.Model):
-    _inherit = _name = 'account.invoice'
-
-    @api.multi
-    def _do_validate(self):
-        # Do the real work inside an iterator so that we can use the
-        # until_timeout.
-        def _validate():
-            report_progress(
-                _("Validating the invoices. This may take several "
-                  "minutes depending on the length of the invoices "),
-                valuemin=0,
-                valuemax=len(self)
-            )
-            records = self.sorted(lambda invoice: len(invoice.invoice_line_ids))
-            for progress, record in enumerate(records):
-                report_progress(progress=progress)
-                with self.env.cr.savepoint():
-                    # Since the soft time-limit may occur after the first
-                    # invoice was validate, but before the second finishes, we
-                    # must ensure all DB changes for each invoice is
-                    # 'atomically' done or not done at all.
-                    record.action_invoice_open()
-                yield record
-
-        res = list(until_timeout(_validate()))
-        if not res and any(self):
-            raise ValidationError(_("No invoice could be validated"))
-        else:
-            return CLOSE_PROGRESS_BAR

xopgi/xopgi_account_celery/invoice8.py
@@ -1,69 +1,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# ---------------------------------------------------------------------
-# invoice
-# ---------------------------------------------------------------------
-# Copyright (c) 2017 Merchise Autrement [~º/~] and Contributors
-# All rights reserved.
-#
-# This is free software; you can redistribute it and/or modify it under the
-# terms of the LICENCE attached (see LICENCE file) in the distribution
-# package.
-#
-# Created on 2017-02-27
-
-from __future__ import (division as _py3_division,
-                        print_function as _py3_print,
-                        absolute_import as _py3_abs_import)
-
-from xoeuf import models, api, MAJOR_ODOO_VERSION
-from xoeuf.odoo import _
-from xoeuf.odoo.jobs import report_progress, until_timeout
-from xoeuf.odoo.exceptions import ValidationError
-
-from .base import CLOSE_PROGRESS_BAR
-
-# The only difference of any importance to us between Odoo 8 and Odoo 9, is
-# how to get the invoice's lines.
-if MAJOR_ODOO_VERSION == 8:
-    def get_lines(invoice):
-        return invoice.invoice_line
-
-elif MAJOR_ODOO_VERSION == 9:
-    def get_lines(invoice):
-        return invoice.invoice_line_ids
-
-else:
-    raise NotImplemented
-
-
-class ValidateInvoice(models.Model):
-    _inherit = _name = 'account.invoice'
-
-    @api.multi
-    def _do_validate(self):
-        # Do the real work inside an iterator so that we can use the
-        # until_timeout.
-        def _validate():
-            report_progress(
-                _("Validating the invoices. This may take several "
-                  "minutes depending on the length of the invoices "),
-                valuemin=0,
-                valuemax=len(self)
-            )
-            records = self.sorted(lambda invoice: len(get_lines(invoice)))
-            for progress, record in enumerate(records):
-                report_progress(progress=progress)
-                with self.env.cr.savepoint():
-                    # Since the soft time-limit may occur after the first
-                    # invoice was validate, but before the second finishes, we
-                    # must ensure all DB changes for each invoice is
-                    # 'atomically' done or not done at all.
-                    record.signal_workflow('invoice_open')
-                yield record
-
-        res = list(until_timeout(_validate()))
-        if not res and any(self):
-            raise ValidationError(_("No invoice could be validated"))
-        else:
-            return CLOSE_PROGRESS_BAR

commit b6d899276d6abdd86f3e730e68b147fe9584c980

Remove invalid condition.
Since `res` is just the amount (an integer) of validated invoices, it's not OK
to use `any`.

The problem was introduced in 5f4610c6ac2e4f97d006fd23238dd48678d36c1f.
Before we use `list` instead of `count`, so `res` was a list of invoices.

xopgi/xopgi_account_celery/base.py
@@ -71,7 +71,7 @@ class ValidateInvoice(models.Model):
             return result
 
         res = count(until_timeout(_validate()))
-        if not res and any(self):
+        if not res:
             raise ValidationError(_("No invoice could be validated"))
         else:
             return CLOSE_PROGRESS_BAR